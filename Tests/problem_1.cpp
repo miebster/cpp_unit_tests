ab8e4a7e7fa166c6421a68f01bce7eaf1376d210
miebster <timerbes@gmail.com>
2024-01-23 20:58:48 -0600

    passing unit tests

--- Tests/problem_1.cpp
+++ Tests/problem_1.cpp
@@ -3,24 +3,35 @@
 
 #include "solution_1.h"
 
+
+/*
+vector<vector<int>> result = solution1.threeSum(q1);
+//    for (vector r1 : result) {
+//        for (int r2 : r1) {
+//            std::cout << r2 << ' ';
+//        };
+//        std::cout << std::endl;
+//    };
+ */
+
 TEST_CASE("test_case_1"){
     Solution_1 solution1;
-    vector<int> q1 = { -1,0,1,2,-1,-4 };
-    vector<vector<int>> a1
-    {
-        {-1,-1,2},
-        {-1,0,1},
-    };
+    vector<int> q1 {-1,0,1,2,-1,-4};
+    vector<vector<int>> a1 {{-1,-1,2},{-1,0,1}};
+    CHECK(solution1.threeSum(q1) == a1);
+}
 
-    vector<vector<int>> result = solution1.threeSum(q1);
-    for (vector r1 : result) {
-        for (int r2 : r1) {
-            std::cout << r2 << ' ';
-        };
-        std::cout << std::endl;
-    };
+TEST_CASE("test_case_2"){
+    Solution_1 solution1;
+    vector<int> q1 { 0,1,1 };
+    vector<vector<int>> a1 { };
+    CHECK(solution1.threeSum(q1) == a1);
+}
 
-//    CHECK(solution1.threeSum(q1) == a1);
-
+TEST_CASE("test_case_3"){
+    Solution_1 solution1;
+    vector<int> q1 {0,0,0};
+    vector<vector<int>> a1 {{0,0,0}};
+    CHECK(solution1.threeSum(q1) == a1);
 }
 
